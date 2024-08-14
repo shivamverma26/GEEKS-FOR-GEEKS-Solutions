1606e3e5c393c7a93b58b049f54a0fb6b2315fbf
Shivam Verma <94590743+shivamverma26@users.noreply.github.com>
2024-08-14 22:08:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Common Substring/longest-common-substring.java
+++ Difficulty: Medium/Longest Common Substring/longest-common-substring.java
@@ -1,30 +1,25 @@
 //{ Driver Code Starts
-//Initial Template for Java
+// Initial Template for Java
 
 import java.io.*;
 import java.util.*;
 
-class GFG
-{
-    public static void main(String args[])throws IOException
-    {
+class GFG {
+    public static void main(String args[]) throws IOException {
         BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(read.readLine());
-        while(t-- > 0)
-        {
-            String input[] = read.readLine().trim().split(" ");
-            int n = Integer.parseInt(input[0]);
-            int m = Integer.parseInt(input[1]);
-            
+        while (t-- > 0) {
             String S1 = read.readLine().trim();
             String S2 = read.readLine().trim();
 
             Solution ob = new Solution();
-            System.out.println(ob.longestCommonSubstr(S1, S2, n, m));
+            System.out.println(ob.longestCommonSubstr(S1, S2));
         }
     }
 }
 // } Driver Code Ends
+
+
 
 
 //User function Template for Java
@@ -55,7 +50,9 @@ class Solution{
            return dp[n][m];
      }
      
-    int longestCommonSubstr(String S1, String S2, int n, int m){
+    int longestCommonSubstr(String S1, String S2){
+        int n=S1.length();
+        int m=S2.length();
         // code here
          int dp[][]=new int [n+1][m+1];
         for(int i=0;i<=n;i++)
