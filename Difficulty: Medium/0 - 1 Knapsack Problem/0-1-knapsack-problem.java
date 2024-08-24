5fe9d167b53e055041a31a17961e10e1fba88704
Shivam Verma <94590743+shivamverma26@users.noreply.github.com>
2024-08-24 23:45:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
@@ -1,49 +1,49 @@
 //{ Driver Code Starts
-import java.util.*;
 import java.io.*;
-import java.lang.*;
+import java.util.*;
 
-class gfg
-{
-    public static void main(String args[])throws IOException
-    {
-        //reading input using BufferedReader class
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        
-        //reading total testcases
-        int t = Integer.parseInt(read.readLine());
-        
-        while(t-- > 0)
-        {
-            //reading number of elements and weight
-            int n = Integer.parseInt(read.readLine());
-            int w = Integer.parseInt(read.readLine());
-            
-            int val[] = new int[n];
-            int wt[] = new int[n];
-            
-            String st[] = read.readLine().trim().split("\\s+");
-            
-            //inserting the values
-            for(int i = 0; i < n; i++)
-              val[i] = Integer.parseInt(st[i]);
-             
-            String s[] = read.readLine().trim().split("\\s+"); 
-            
-            //inserting the weigths
-            for(int i = 0; i < n; i++)
-              wt[i] = Integer.parseInt(s[i]);
-              
-            //calling method knapSack() of class Knapsack
-            System.out.println(new Solution().knapSack(w, wt, val, n));
+class gfg {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            int w = Integer.parseInt(br.readLine());
+
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+            List<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] val = new int[array.size()];
+            int idx = 0;
+            for (int i : array) val[idx++] = i;
+
+            String lin = br.readLine();
+            String[] toke = lin.split(" ");
+            List<Integer> array1 = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : toke) {
+                array1.add(Integer.parseInt(token));
+            }
+
+            int[] wt = new int[array1.size()];
+            idx = 0;
+            for (int i : array1) wt[idx++] = i;
+
+            // calling method knapSack() of class Solution
+            System.out.println(new Solution().knapSack(w, wt, val));
         }
     }
 }
-
-
-
-
 // } Driver Code Ends
+
+
 
 
 class Solution 
@@ -67,8 +67,9 @@ class Solution
     }
     
   
-    static int knapSack(int W, int wt[], int val[], int n) 
+    static int knapSack(int W, int wt[], int val[]) 
     { 
+        int n=val.length;
         int[][] dp = new int[n + 1][W + 1];
         
         
