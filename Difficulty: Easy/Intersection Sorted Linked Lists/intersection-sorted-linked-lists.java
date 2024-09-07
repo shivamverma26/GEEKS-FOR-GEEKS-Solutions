17f49673c066990ae23dc4efd3e5ba2e63da932b
Shivam Verma <94590743+shivamverma26@users.noreply.github.com>
2024-09-07 19:11:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Intersection Sorted Linked Lists/intersection-sorted-linked-lists.java
@@ -0,0 +1,105 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+public class Main {
+    public static void printList(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        sc.nextLine();
+        while (T-- > 0) {
+            Node head1 = null, tail1 = null;
+            Node head2 = null, tail2 = null;
+
+            String input1 = sc.nextLine();
+            String[] elems1 = input1.split(" ");
+            for (String elem : elems1) {
+                Node newNode = new Node(Integer.parseInt(elem));
+                if (head1 == null) {
+                    head1 = newNode;
+                    tail1 = newNode;
+                } else {
+                    tail1.next = newNode;
+                    tail1 = newNode;
+                }
+            }
+
+            String input2 = sc.nextLine();
+            String[] elems2 = input2.split(" ");
+            for (String elem : elems2) {
+                Node newNode = new Node(Integer.parseInt(elem));
+                if (head2 == null) {
+                    head2 = newNode;
+                    tail2 = newNode;
+                } else {
+                    tail2.next = newNode;
+                    tail2 = newNode;
+                }
+            }
+
+            Solution obj = new Solution();
+
+            Node result = obj.findIntersection(head1, head2);
+            printList(result);
+        }
+        sc.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+/* Node of a linked list
+ class Node {
+   int data;
+    Node next;
+    Node(int d)  { data = d;  next = null; }
+}
+*/
+
+class Solution {
+    public static Node findIntersection(Node head1, Node head2) {
+        // code here.
+         Set<Integer> set = new HashSet<>();
+        Node temp=head2;
+        while(temp!=null){
+            set.add(temp.data);
+            temp=temp.next;
+        }
+        temp=head1;
+        Node head =null;
+        Node last = head;
+        while(temp!=null){
+            if(set.contains(temp.data)){
+                Node newnode = new Node(temp.data);
+                if(head == null){
+                    head = newnode;
+                    last = head;
+                }else{
+                    last.next = newnode;
+                    last = newnode;
+                }
+            }
+            temp=temp.next;
+        }
+        return head;
+    }
+}
