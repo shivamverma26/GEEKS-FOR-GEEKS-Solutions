3d54d02beecbed5de040eb87dee839bcceaf2204
Shivam Verma <94590743+shivamverma26@users.noreply.github.com>
2024-09-10 12:47:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Merge two sorted linked lists/merge-two-sorted-linked-lists.java
@@ -0,0 +1,108 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+public class Main {
+    public static void printList(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        sc.nextLine();
+        while (T-- > 0) {
+            Node head1 = null, tail1 = null;
+            Node head2 = null, tail2 = null;
+
+            String input1 = sc.nextLine();
+            String[] elems1 = input1.split(" ");
+            for (String elem : elems1) {
+                Node newNode = new Node(Integer.parseInt(elem));
+                if (head1 == null) {
+                    head1 = newNode;
+                    tail1 = newNode;
+                } else {
+                    tail1.next = newNode;
+                    tail1 = newNode;
+                }
+            }
+
+            String input2 = sc.nextLine();
+            String[] elems2 = input2.split(" ");
+            for (String elem : elems2) {
+                Node newNode = new Node(Integer.parseInt(elem));
+                if (head2 == null) {
+                    head2 = newNode;
+                    tail2 = newNode;
+                } else {
+                    tail2.next = newNode;
+                    tail2 = newNode;
+                }
+            }
+
+            Solution obj = new Solution();
+            Node head = obj.sortedMerge(head1, head2);
+            printList(head);
+        }
+        sc.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+/*
+  Merge two linked lists
+  head pointer input could be NULL as well for empty list
+  Node is defined as
+    class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+*/
+
+class Solution {
+    Node sortedMerge(Node head1, Node head2) {
+        Node temp = new Node(0);
+        Node ans = temp;
+        
+        while (head1 != null && head2 != null) {
+            if (head1.data <= head2.data) {
+                temp.next = head1;
+                head1 = head1.next;
+            } else {
+                temp.next = head2;
+                head2 = head2.next;
+            }
+            temp = temp.next;
+        }
+        
+        if (head1 != null) {
+            temp.next = head1;
+        }
+        
+        if (head2 != null) {
+            temp.next = head2;
+        }
+        
+        return ans.next;
+    }
+}
+
