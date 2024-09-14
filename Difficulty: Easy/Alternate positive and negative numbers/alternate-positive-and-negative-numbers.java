30877d24421723cadd31b81e1bb589729e6d82d7
Shivam Verma <94590743+shivamverma26@users.noreply.github.com>
2024-09-14 23:13:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Alternate positive and negative numbers/alternate-positive-and-negative-numbers.java
+++ Difficulty: Easy/Alternate positive and negative numbers/alternate-positive-and-negative-numbers.java
@@ -1,10 +1,8 @@
 //{ Driver Code Starts
-//Initial Template for Java
+// Initial Template for Java
 
-
-
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
 
@@ -12,17 +10,17 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int tc = Integer.parseInt(br.readLine().trim());
         while (tc-- > 0) {
-            String[] inputLine;
-            int n = Integer.parseInt(br.readLine().trim());
-            int[] arr = new int[n];
-            inputLine = br.readLine().trim().split(" ");
-            for (int i = 0; i < n; i++) {
-                arr[i] = Integer.parseInt(inputLine[i]);
+            String input = br.readLine();
+            String[] inputArray = input.split("\\s+");
+            ArrayList<Integer> arr = new ArrayList<>();
+
+            for (String s : inputArray) {
+                arr.add(Integer.parseInt(s));
             }
 
-            new Solution().rearrange(arr, n);
-            for (int i = 0; i < n; i++) {
-                System.out.print(arr[i] + " ");
+            new Solution().rearrange(arr);
+            for (int num : arr) {
+                System.out.print(num + " ");
             }
             System.out.println();
         }
@@ -32,31 +30,40 @@ public class Main {
 // } Driver Code Ends
 
 
-//User function Template for Java
-
-
-
+// User function Template for Java
 
 class Solution {
-    void rearrange(int arr[], int n) {
-        // code here
-         ArrayList<Integer> neg = new ArrayList<Integer>();
-        ArrayList<Integer> pos = new ArrayList<Integer>();
+    void rearrange(ArrayList<Integer> arr) {
+        ArrayList<Integer> neg = new ArrayList<>();
+        ArrayList<Integer> pos = new ArrayList<>();
         
-        for (int i = 0; i < n; i++) {
-            if (arr[i] < 0)
-                neg.add(arr[i]);
+        // Separate positive and negative numbers
+        for (int num : arr) {
+            if (num < 0)
+                neg.add(num);
             else
-                pos.add(arr[i]);
+                pos.add(num);
         }
-
+        
         int i = 0, j = 0, k = 0;
+        
+        // Clear original list to refill it
+        arr.clear();
+        
+        // Merge positive and negative numbers alternately
         while (i < neg.size() && j < pos.size()) {
-            arr[k++] = pos.get(j++);
-            arr[k++] = neg.get(i++); 
+            arr.add(pos.get(j++));
+            arr.add(neg.get(i++));
         }
-        while (j < pos.size()) { arr[k++] = pos.get(j++); }
-
-        while (i < neg.size()) { arr[k++] = neg.get(i++); }
+        
+        // Append remaining positive numbers
+        while (j < pos.size()) {
+            arr.add(pos.get(j++));
+        }
+        
+        // Append remaining negative numbers
+        while (i < neg.size()) {
+            arr.add(neg.get(i++));
+        }
     }
 }
