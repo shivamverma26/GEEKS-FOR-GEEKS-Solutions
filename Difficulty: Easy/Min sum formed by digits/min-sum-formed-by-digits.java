4df924dcd2fcfa68b05d745b01daf7183b7dffd5
Shivam Verma <94590743+shivamverma26@users.noreply.github.com>
2024-09-14 12:07:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Min sum formed by digits/min-sum-formed-by-digits.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int a[] = new int[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            
+            Solution obj = new Solution();
+            System.out.println(obj.minSum(a, n));
+            
+        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    
+    public static long minSum(int arr[], int n)
+    {
+        if (n == 0) return 0;
+        if (n == 1) return arr[0];
+
+        Arrays.sort(arr);
+        
+        long num1 = 0, num2 = 0;
+        
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                num1 = num1 * 10 + arr[i];
+            } else {
+                num2 = num2 * 10 + arr[i];
+            }
+        }
+        
+        return num1 + num2;
+    }
+}
