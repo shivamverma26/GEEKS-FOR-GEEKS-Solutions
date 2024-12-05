6bc0924024c256b5e7d39b9cce01ffa67fc61328
Shivam Verma <94590743+shivamverma26@users.noreply.github.com>
2024-12-05 23:12:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
+++ Difficulty: Easy/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
@@ -11,11 +11,9 @@ class GFG {
         while (t-- > 0) {
             String input = br.readLine();
             String[] inputArray = input.split("\\s+");
-            ArrayList<Integer> a = new ArrayList<>();
+            int a[] = new int[inputArray.length];
 
-            for (String s : inputArray) {
-                a.add(Integer.parseInt(s));
-            }
+            for (int i = 0; i < a.length; i++) a[i] = Integer.parseInt(inputArray[i]);
 
             Solution ob = new Solution();
             ob.sort012(a);
@@ -24,44 +22,41 @@ class GFG {
                 System.out.print(num + " ");
             }
             System.out.println();
+            System.out.println("~");
         }
     }
 }
+
 
 // } Driver Code Ends
 
-
-//User function template for Java
-
-class Solution
-{
-    public static void swap(ArrayList<Integer> a, int i,int j)
-    {
-
-        int tmp=a.get(i);
-        a.set(i,a.get(j));
-        a.set(j,tmp);
-        
-    }
-    public static void sort012(ArrayList<Integer> a)
-    {
-        // code here 
-        int n=a.size();
-        int low=0;
-        int high=n-1;
-        int mid=0;
-        while(mid<=high){
-            if(a.get(mid)==0){
-                swap(a,mid,low);
-                mid++;
-                low++;
-            }
-            else if(a.get(mid)==1) mid++;
-            else{
-                swap(a,mid,high);
-                high--;
+ class Solution {
+    public void sort012(int[] arr) {
+        int n = arr.length;
+        int low = 0, mid = 0, high = n - 1;
+        while (mid <= high) {
+            switch (arr[mid]) {
+                case 0:
+                    swap(arr, low++, mid++);
+                    break;
+                case 1:
+                    mid++;
+                    break;
+                case 2:
+                    swap(arr, mid, high--);
+                    break;
             }
         }
-        
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
+
+
+
+//{ Driver Code Starts.
+// } Driver Code Ends
